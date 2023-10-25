e286c3dd571857511a08f372ad2e0f88304ec869
jungyn <106477654+jungyn@users.noreply.github.com>
2023-10-25 14:00:43 +0900

    [level 1] Title: 모의고사, Time: 2.63 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,20 @@
+def solution(answers):
+    answer = []
+    score = [0,0,0]
+    
+    s1 = [1,2,3,4,5]
+    s2 = [2,1,2,3,2,4,2,5]
+    s3 = [3,3,1,1,2,2,4,4,5,5]
+    
+    for i in range(len(answers)):
+        if answers[i] == s1[i%5]:
+            score[0] += 1
+        if answers[i] == s2[i%8]:
+            score[1] += 1
+        if answers[i] == s3[i%10]:
+            score[2] += 1
+    
+    for idx, num in enumerate(score):
+        if num == max(score):
+            answer.append(idx+1)
+    return answer

0e4c2c9a64449072e930923a1f430dc97d94819b
jungyn <106477654+jungyn@users.noreply.github.com>
2024-08-05 17:10:35 +0900

    [level 2] Title: 숫자의 표현, Time: 4.17 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -2,10 +2,9 @@ def solution(n):
     answer = 0
     for i in range(1, n+1):
         sum = 0
-        j = i
         while sum < n:
-            sum += j
-            j += 1
+            sum += i
+            i += 1
             if sum == n:
                 answer += 1
     return answer

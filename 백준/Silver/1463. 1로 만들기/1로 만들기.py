a15448519bcccc9c7a98b07cb621b034708f305b
jungyn <106477654+jungyn@users.noreply.github.com>
2024-11-19 16:18:21 +0900

    [Silver III] Title: 1로 만들기, Time: 476 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+d = [0] * (n+1)
+d[1] = 0
+for i in range(2, n+1):
+    if i%2 == 0 and i%3 == 0:
+        d[i] = min(d[i-1], d[i//2], d[i//3]) + 1
+    elif i%2 == 0 and i%3 != 0:
+        d[i] = min(d[i-1], d[i//2]) + 1
+    elif i%3 == 0 and i%2 != 0:
+        d[i] = min(d[i-1], d[i//3]) + 1
+    else:
+        d[i] = d[i-1] + 1
+print(d[n])

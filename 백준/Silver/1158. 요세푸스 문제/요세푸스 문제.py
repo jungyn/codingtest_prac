ec3a6c3437707194697cf9f73e43b918cc8eb512
jungyn <106477654+jungyn@users.noreply.github.com>
2024-11-19 15:07:05 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 1800 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+
+from collections import deque
+
+queue = deque([i for i in range(1, n+1)])
+
+print('<', end='')
+while True:
+    for _ in range(k-1):
+        queue.append(queue.popleft())
+    print(queue.popleft(), end = '')
+    if queue:
+        print(', ', end='')
+    else:
+        break
+print('>', end='')

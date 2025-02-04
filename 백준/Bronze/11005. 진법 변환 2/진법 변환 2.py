df11844295329bae4b2c18f529e3554b0bc20780
jungyn <106477654+jungyn@users.noreply.github.com>
2025-02-04 09:53:16 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 37004 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -0,0 +1,13 @@
+n, b = map(int, input().split())
+
+import string
+
+tmp = string.digits+string.ascii_lowercase
+def convert(num, base) :
+    q, r = divmod(num, base)
+    if q == 0 :
+        return tmp[r] 
+    else :
+        return convert(q, base) + tmp[r]
+
+print(convert(n, b).upper())

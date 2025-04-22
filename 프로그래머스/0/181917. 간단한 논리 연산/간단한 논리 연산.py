d5231a0b0b5549ad85356abc7bbc63c683a0a597
jungyn <106477654+jungyn@users.noreply.github.com>
2025-04-22 15:56:18 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,16 +1,3 @@
 def solution(x1, x2, x3, x4):
-    if x1 == False and x2 == False:
-        cond1 = False
-    else:
-        cond1 = True
-        
-    if x3 == False and x4 == False:
-        cond2 = False
-    else:
-        cond2 = True
-    
-    if cond1 == True and cond2 == True:
-        answer = True
-    else:
-        answer = False
+    answer = (x1 | x2) & (x3 | x4)
     return answer
